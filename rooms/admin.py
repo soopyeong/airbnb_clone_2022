66d0b4f8badfcd08b27ccd9832b030037ffdbf9e
soopyeong <rndtn2382@naver.com>
2022-04-15 19:32:38 +0900

    Arrange the Admin Panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,56 @@ class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "city", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {"fields": ("amenities", "facilities", "house_rules")},
+        ),
+        ("Last Details", {"fields": ("host", "room_type")}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "count_amenities",
+    )
+
+    list_filter = (
+        "instant_book",
+        "city",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
 
 
 @admin.register(models.Photo)
